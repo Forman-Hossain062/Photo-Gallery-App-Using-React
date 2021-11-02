0a547211de9fbaff692b408ed65b487f94f5bf29
Forman Hossain <68132944+Forman-Hossain062@users.noreply.github.com>
2021-11-02 19:52:17 +0600

    finall Project

--- src/Components/MainComponents.js
+++ src/Components/MainComponents.js
@@ -1,71 +1,71 @@
-import React, { Component } from 'react'
-import Body from './body/body';
-import Footer from '../Components/footer/footer';
-import Header from './header/Header';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import Auth from './Auth/Auth';
-import Logout from '../Components/Auth/Logout'
-import { connect } from 'react-redux';
-import { authCheck } from '../redux/authActionCreators';
-import Breakfast from './body/Catagories/Breakfast/Breakfast';
-import Lunch from './body/Catagories/Lunch/Lunch';
-import Shakes from './body/Catagories/Shakes/Shakes';
+import React, { Component } from "react";
+import Body from "./body/body";
+// import Footer from "../Components/footer/footer";
+import Header from "./header/Header";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Auth from "./Auth/Auth";
+import Logout from "../Components/Auth/Logout";
+import { connect } from "react-redux";
+import { authCheck } from "../redux/authActionCreators";
+import Breakfast from "./body/Catagories/Breakfast/Breakfast";
+import Lunch from "./body/Catagories/Lunch/Lunch";
+import Shakes from "./body/Catagories/Shakes/Shakes";
 
-const mapStateToProps = state => {
-    // console.log(state);
-    return {
-        token: state.dishes.token,
-    }
-}
-const mapDispatchToProps = dispatch => {
-    return {
-        authCheck: () => dispatch(authCheck())
-    }
-}
+const mapStateToProps = (state) => {
+  // console.log(state);
+  return {
+    token: state.dishes.token,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    authCheck: () => dispatch(authCheck()),
+  };
+};
 class MainComponents extends Component {
-
-    componentDidMount() {
-        // console.log(this.props);
-        this.props.authCheck();
+  componentDidMount() {
+    // console.log(this.props);
+    this.props.authCheck();
+  }
+  render() {
+    // console.log(this.props);
+    let links = null;
+    if (this.props.token === null) {
+      links = (
+        <Switch>
+          <Route path="/login" exact component={Auth} />
+          <Route path="/" exact component={Body} />
+          <Route path="/breakfast" exact component={Breakfast} />
+          <Route path="/lunch" exact component={Lunch} />
+          <Route path="/shakes" exact component={Shakes} />
+          <Redirect to="/login" />
+        </Switch>
+      );
+    } else {
+      links = (
+        <Switch>
+          <Route path="/" exact component={Body} />
+          <Route path="/breakfast" exact component={Breakfast} />
+          <Route path="/lunch" exact component={Lunch} />
+          <Route path="/shakes" exact component={Shakes} />
+          <Route path="/logout" exact component={Logout} />
+          <Redirect to="/" />
+        </Switch>
+      );
     }
-    render() {
-        // console.log(this.props);
-        let links = null;
-        if (this.props.token === null) {
-            links = (
-                <Switch>
-                    <Route path="/login" exact component={Auth} />
-                    <Route path="/" exact component={Body} />
-                    <Route path="/breakfast" exact component={Breakfast} />
-                    <Route path="/lunch" exact component={Lunch} />
-                    <Route path="/shakes" exact component={Shakes} />
-                    <Redirect to="/login" />
-                </Switch>
-            )
-        }
-        else {
-            links = (
-                <Switch>
-                    <Route path="/" exact component={Body} />
-                    <Route path="/breakfast" exact component={Breakfast} />
-                    <Route path="/lunch" exact component={Lunch} />
-                    <Route path="/shakes" exact component={Shakes} />
-                    <Route path="/logout" exact component={Logout} />
-                    <Redirect to="/" />
-                </Switch>
-            )
-        }
-        return (
+    return (
+      <div
+        style={{
+          backgroundColor: "#E8F6EF",
+          minHeight: "100vh",
+        }}
+      >
+        <Header />
+        <span> {links}</span>
 
-            <div style={{
-                backgroundColor: "#E8F6EF",
-            }}>
-                <Header />
-                {links}
-
-                <Footer />
-            </div>
-        )
-    }
+        {/* <Footer /> */}
+      </div>
+    );
+  }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(MainComponents);
--- src/Components/body/DishDetail.js
+++ src/Components/body/DishDetail.js
@@ -1,36 +1,47 @@
-import React, { Component } from 'react'
-import { Modal, CardText, CardFooter, ModalBody, Card, CardBody, CardImg, ModalFooter, ModalHeader, Button } from 'reactstrap';
-import LoadComments from './LoadComments';
-import CommentForm from './CommentForm';
+import React, { Component } from "react";
+import { CardText, Card, CardBody, CardImg } from "reactstrap";
+import LoadComments from "./LoadComments";
+import CommentForm from "./CommentForm";
 
 class DishDetail extends Component {
+  render() {
+    return (
+      <div>
+        <Card
+          style={{
+            textAlign: "left",
+            textTransform: "capitalize",
+          }}
+          className="cardAnim"
+        >
+          <CardImg
+            src={"http://localhost:3002/" + this.props.dish.img}
+            alt={this.props.dish.title}
+            className="ModalImg"
+          />
 
-    render() {
-        return (
-            <div>
-                <Card style={{ textAlign: 'left', textTransform: 'capitalize' }} className=''>
-
-                    <CardImg src={"http://localhost:3002/" + this.props.dish.img} alt={this.props.dish.title} className='ModalImg' />
-
-                    <CardBody>
-                        <p style={{ fontWeight: '1.2rem' }}>Name: {this.props.dish.title} </p>
-                        <p>Price: {this.props.dish.price} $</p>
-                        <CardText> Description: {this.props.dish.desc}</CardText>
-                    </CardBody>
-                    <hr />
-                    <CardText>
-                        <LoadComments comments={this.props.comments}
-                            commentsIsLoading={this.props.commentsIsLoading} />
-                    </CardText>
-                    <CommentForm
-                        addComment={this.props.addComment}
-                        dishId={this.props.dish.id}
-                    />
-                </Card>
-
-            </div>
-        )
-    }
+          <CardBody>
+            <p style={{ fontWeight: "1.2rem" }}>
+              Name: {this.props.dish.title}{" "}
+            </p>
+            <p>Price: {this.props.dish.price} $</p>
+            <CardText> Description: {this.props.dish.desc}</CardText>
+          </CardBody>
+          <hr />
+          <CardText>
+            <LoadComments
+              comments={this.props.comments}
+              commentsIsLoading={this.props.commentsIsLoading}
+            />
+          </CardText>
+          <CommentForm
+            addComment={this.props.addComment}
+            dishId={this.props.dish.id}
+          />
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default DishDetail;
